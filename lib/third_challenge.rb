35d08f2be0f2bc4e9c5389d378fc2a5fd5b2ef73
Trey Jennings <tjennings343@gmail.com>
2020-08-28 14:17:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         name: "Lord Capulet",
@@ -40,5 +40,5 @@ def third_challenge
    }
   }
 
-  
+
 end
